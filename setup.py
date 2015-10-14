aeb8603354e04ef7149ad75b4bc5335a7f9ac9ba
Buck Golemon <buck@yelp.com>
2015-10-14 13:44:35 -0700

    fix the README.md installation failure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="hodor",
-    version='0',
+    version='1!0',
     description='hodor!',
     long_description=open('README.md').read(),
     classifiers=[
