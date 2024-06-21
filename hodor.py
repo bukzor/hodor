3c6ad6b83d708cb69193e4ae4df44e918db36212
Buck Evan <buck.evan@sentry.io>
2024-06-21 15:59:17 -0500

    hodor (port to python3.12)

--- hodor.py
+++ hodor.py
@@ -1,43 +1,48 @@
 """hodor"""
+
 # pylint:disable=invalid-name
 from __future__ import print_function
 
 
 def metaclass(cls, mcs):
     "py2/py3 compat, hodor"
-    return mcs('hodor', cls.__bases__, dict(vars(cls)))
+    return mcs("hodor", cls.__bases__, dict(vars(cls)))
 
 
 class hodor(type):
     """hodor"""
+
     def __repr__(cls):
-        return 'hodor'
+        return "hodor"
 
     def __call__(cls, *args):
         return cls
+
 
 # hodor!
 hodor = metaclass(hodor, hodor)
 hodor.__class__ = hodor  # hodor.
 
 import operator
+
 for op, val in vars(operator).items():
-    if not op.startswith('__') or isinstance(val, str):
+    if not op.startswith("__") or isinstance(val, str) or op == "__call__":
         continue
     setattr(hodor, op, hodor)
-    setattr(hodor, op.replace('__', '__r', 1), hodor)
+    setattr(hodor, op.replace("__", "__r", 1), hodor)
 
-for op in ('getattr', 'setattr', 'delattr'):
-    setattr(hodor, '__%s__' % op, hodor)
+for op in ("getattr", "setattr", "delattr"):
+    setattr(hodor, "__%s__" % op, hodor)
 
 # hodor...
 import sys
+
 _ = sys.modules[__name__]  # prevent module gc in python 2
 sys.modules[__name__] = hodor
 
-__all__ = ('hodor',)
+__all__ = ("hodor",)
 
 main = hodor
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(main())
--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 """hodor"""
+
 from setuptools import setup
 
 setup(
     name="hodor",
-    version='1!0',
-    description='hodor!',
-    long_description=open('README.md').read(),
-    classifiers=[
-    ],
+    version="2!0",
+    description="hodor!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[],
     author="Buck Evan",
     author_email="buck.2019@gmail.com",
     url="http://github.com/bukzor/hodor",
-    license='hodor',
-    py_modules=['hodor'],
-    platforms=['hodor'],
+    license="hodor",
+    py_modules=["hodor"],
+    platforms=["hodor"],
     entry_points={
-        'console_scripts': [
-            'hodor = hodor:hodor',
+        "console_scripts": [
+            "hodor = hodor:hodor",
         ],
     },
 )
